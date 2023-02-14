df51cf47320103c46eb1c3ad31c152fcadefd379
matt-mert <mertkucukakinci@gmail.com>
2023-02-14 04:07:53 +0300

    Initial configurations

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    
+    private Rigidbody playerRb;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        playerRb.velocity = Vector3.forward;
+    }
 }
